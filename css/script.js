b7ea90d52be3117a6a70a7bfd40f0f2bfe71e18e
Jacobo0711 <128314581+Jacobo0711@users.noreply.github.com>
2023-05-18 06:50:42 -0500

    Update script.js

--- css/script.js
+++ css/script.js
@@ -1,15 +1,1 @@
-document.addEventListener("DOMContentLoaded", function() {
-    var loginForm = document.querySelector(".login-form");
-    var usernameInput = document.getElementById("username");
-    var passwordInput = document.getElementById("password");
 
-    loginForm.addEventListener("submit", function(event) {
-        event.preventDefault(); 
-        var username = usernameInput.value;
-        var password = passwordInput.value;
-
-        console.log("Nombre de usuario: " + username);
-        console.log("Contraseña: " + password);
-        window.location.href = "ruta-a-tu-pagina-principal.html";
-    });
-});
